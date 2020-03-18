445a677855e9d168edb3a714954c322cf843b681
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-03-18 13:28:10 -0400

    follows lecture and creates a quick sort as well as a merge sort

---
+++ src/mergesort.py
@@ -0,0 +1,80 @@
+# Merge Sort as followed in Lecture:
+
+# ***UNDERSTAND***
+"""merge sort
+
+1. split the list in half
+2. split again, and again. Continue the process until we are working with many 'sorted' lists with len(1)
+3.
+[8] i
+[] k
+[6] j
+---------- compare and merge, over and over
+vvv[8] i
+[6 8] k
+^^^[6] j
+
+[6 8] i
+[ ] k
+[4 5] j
+
+vvv[6 8] i
+[4 5 6 8] k
+^^^[4 5] j
+
+k is the subarray because we are preallocating space
+"""
+
+
+def merge(lstA, lstB):
+    elements = len(lstA) + len(lstB)
+    merged_lst = [0] * elements
+    # put back together here
+    # sorting happens here
+    a = 0  # cursor for list a
+    b = 0  # cursor for list b
+
+    for k in range(0, elements):
+        # k is our cursor for the sub array
+        # compare a and b
+        # if a is out of range, push b and iterate
+        if a >= len(lstA):  # we're done with a, push remainder of b
+            merged_lst[k] = lstB[b]
+            b += 1
+        # if b is out of range, push b and iterate
+        elif b >= len(lstB):  # we're done with b, push remainder of a
+            merged_lst[k] = lstA[a]
+            a += 1
+        # if a is smaller, put it in array and iterate a & k
+        elif lstA[a] < lstB[b]:
+            merged_lst[k] = lstA[a]
+            a += 1
+        # if b is smaller put it in array and iterate b & k
+        else:
+            merged_lst[k] = lstB[b]
+            b += 1
+
+    return merged_lst
+
+
+def mergesort(lst):
+    # split here
+    # base condition
+    # if array size > 1
+    # find the middle of the list
+    if len(lst) > 1:
+        # # sort stuff in left and put stuff to the left in left
+        left = mergesort(lst[: len(lst) // 2])
+    # sort stuff in right and put the stuff to the right in right
+        right = mergesort(lst[len(lst) // 2:])
+
+        lst = merge(left, right)
+    # merge left and right
+
+    return lst
+
+
+my_list = [8, 6, 5, 4, 45, 23, 63, 84, 24, 1, 333,
+           65, 349, 923, 9534, 4782, 39886, 2, 29, 93, 549]
+
+print(mergesort(my_list))
---
+++ src/quicksort.py
@@ -0,0 +1,28 @@
+# Quick Sort during lecture:
+
+
+def quicksort(data):
+    # base case: if array length 0 or 1 - explicitly defined as sorted, because 1 is sorted and 0 is nothing
+    if len(data) < 2:
+        return data
+    else:
+        # pick a pivot might as well pick first because its unsorted, none are better
+        pivot = data[0]
+        left = []
+        right = []
+        # put anything smaller into left array
+        # put anything bigger into right array
+        # do not include pivot - which we have decided is the first value
+        for value in data[1:]:
+            if value <= pivot:
+                left.append(value)
+            else:
+                right.append(value)
+        # return quicksort(left) + pivot + quicksort(right)
+        # we put pivot in brackets so we are adding array + array + array, because pivot would otherwise be an int
+        return quicksort(left) + [pivot] + quicksort(right)
+
+
+my_list = [2, 5, 3, 7, 4, 8, 4, 2, 6, 44, 3, 77, 454, 23, 54, 1]
+
+print(quicksort(my_list))
